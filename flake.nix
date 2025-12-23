6bed62685a1b76f4499ad937a71b715a737f53f4
Steven <steven@Stevens-Mac-mini.local>
2025-12-23 11:03:43 +0800

    ghosttyconfig

--- flake.nix
+++ flake.nix
@@ -40,6 +40,7 @@
 	      pkgs.lazygit
 	      pkgs.gh
 	      pkgs.git
+	      pkgs.zoxide
 	    ];
 
 	    home.sessionPath = [
@@ -47,22 +48,38 @@
 	    ];
 
             programs.zsh.enable = true;
+
+	    programs.ghostty = {
+	      enable = true;
+	      package = if pkgs.stdenv.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
+	      enableZshIntegration = true;
+
+	      settings = {
+		command = "/run/current-system/sw/bin/tmux new -As0";
+
+		background-opacity = "0.9";
+		background-blur = true;
+		
+		window-padding-x = 0;
+		window-padding-y = 0;
+		window-padding-balance = true;
+		window-padding-color = "extend";
+		window-decoration = false;
+
+		font-size = 20;
+	      };
+	    };
+
             home.stateVersion = "26.05";
           };
           # List packages installed in system profile. To search by name, run:
           # $ nix-env -qaP | grep wget
           environment.systemPackages =
            [
- 	pkgs.ghostty-bin
  	pkgs.aerospace
  	pkgs.raycast
  	zenWithPolicies
            ];
-
-
-      programs.zsh = {
-        enable = true;
-      };
 
       programs.tmux = {
         enable = true;
