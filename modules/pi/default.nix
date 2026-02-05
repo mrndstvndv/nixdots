9451f8aab44c1feb7ffc60f8a88b5c44d60d09a3
mrndstvndv <stvndvmrnd@gmail.com>
2026-02-05 14:06:07 +0000

    pi: use rsync for extensions to fix node_modules resolution

--- modules/pi/default.nix
+++ modules/pi/default.nix
@@ -1,9 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home.file.".pi/agent/AGENTS.md".source = ./AGENTS.md;
   home.file.".pi/agent/settings.json".source = ./settings.json;
-  home.file.".pi/packages/nixdots-extensions" = {
-    source = ./package;
-    recursive = true;
-  };
+
+  # Copy package to ~/.pi/packages/nixdots-extensions to allow node_modules to work
+  # (Symlinks resolve to Nix store where node_modules doesn't exist)
+  home.activation.installPiExtensions = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    pkgDir="$HOME/.pi/packages/nixdots-extensions"
+    mkdir -p "$pkgDir"
+    
+    # Sync files from store, overwriting existing ones but preserving node_modules
+    # We use rsync to efficiently update and handle permissions
+    ${pkgs.rsync}/bin/rsync -a --chmod=u+w --exclude=node_modules --exclude=package-lock.json ${./package}/ $pkgDir/
+  '';
 }
