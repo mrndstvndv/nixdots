11c8df6a9af2d7e44a46edabe8bf05222dfdc018
mrndstvndv <stvndvmrnd@gmail.com>
2026-02-05 13:33:19 +0000

    pi: restructure extensions as local package

--- modules/pi/default.nix
+++ modules/pi/default.nix
@@ -2,5 +2,5 @@
 {
   home.file.".pi/agent/AGENTS.md".source = ./AGENTS.md;
   home.file.".pi/agent/settings.json".source = ./settings.json;
-  home.file.".pi/agent/extensions".source = ./extensions;
+  home.file.".pi/packages/nixdots-extensions".source = ./package;
 }
