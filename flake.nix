a81ffdbdbd6e53b91e1f8a7aff24235b54d9fbe2
mrndstvndv <stvndvmrnd@gmail.com>
2026-02-09 17:19:27 +0800

    nix: automatic optimizations

--- flake.nix
+++ flake.nix
@@ -75,6 +75,7 @@
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
+      nix.optimise.automatic = true;
 
       # Enable alternative shell support in nix-darwin.
       # programs.fish.enable = true;
