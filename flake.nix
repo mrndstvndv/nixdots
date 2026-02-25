db923c0fd044d0746c8d3409822ca22eebb1c243
steven <steven@proputer.local>
2026-02-25 10:01:04 +0800

    pkgs: remove zen

--- flake.nix
+++ flake.nix
@@ -5,8 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    zen.url = "github:0xc000022070/zen-browser-flake";
-    zen.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     my-neovim.url = "github:crimera/nvim.config";
@@ -44,42 +42,27 @@
     };
   };
 
-   outputs = inputs@{ self, nix-darwin, nixpkgs, zen, home-manager, my-neovim, opencode, amp, helium, codex, nix-homebrew, homebrew-core, homebrew-cask, homebrew-smctemp, nix-on-droid }:
+   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, my-neovim, opencode, amp, helium, codex, nix-homebrew, homebrew-core, homebrew-cask, homebrew-smctemp, nix-on-droid }:
    let
-      configuration = { pkgs, zen, home-manager, nixpkgs, ... }:
-       let
-         zenWithPolicies =
-           pkgs.wrapFirefox
-             (zen.packages.aarch64-darwin.beta-unwrapped.override {
-               policies = {
-                 DisableAppUpdate = true;
-                 DisableTelemetry = true;
-               };
-             })
-             {
-               icon = "zen-browser";
-             };
-       in
-        {
-          imports = [ inputs.home-manager.darwinModules.home-manager ];
-          users.users.steven = {
-            name = "steven";
-            home = "/Users/steven";
-          };
-            home-manager.extraSpecialArgs = { inherit (inputs) my-neovim opencode amp helium codex; };
-           home-manager.backupFileExtension = "backup";
-          home-manager.users.steven = {
-            imports = [
-              ./darwin/common.nix
-              ./darwin/home.nix
-            ];
-          };
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
-          environment.systemPackages =
-           [
- 	zenWithPolicies
+      configuration = { pkgs, home-manager, nixpkgs, ... }:
+       {
+         imports = [ inputs.home-manager.darwinModules.home-manager ];
+         users.users.steven = {
+           name = "steven";
+           home = "/Users/steven";
+         };
+         home-manager.extraSpecialArgs = { inherit (inputs) my-neovim opencode amp helium codex; };
+         home-manager.backupFileExtension = "backup";
+         home-manager.users.steven = {
+           imports = [
+             ./darwin/common.nix
+             ./darwin/home.nix
            ];
+         };
+
+         # List packages installed in system profile. To search by name, run:
+         # $ nix-env -qaP | grep wget
+         environment.systemPackages = [];
 
       # For darwin-nix to work on determinate nix
       nix.enable = false;
@@ -106,7 +89,7 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#proputer
     darwinConfigurations."proputer" = nix-darwin.lib.darwinSystem {
-      specialArgs = { inherit zen home-manager; inherit (inputs) my-neovim homebrew-nix homebrew-core homebrew-cask homebrew-smctemp codex; };
+      specialArgs = { inherit home-manager; inherit (inputs) my-neovim homebrew-nix homebrew-core homebrew-cask homebrew-smctemp codex; };
       modules = [ 
         inputs.nix-homebrew.darwinModules.nix-homebrew
         ./modules/nix-homebrew.nix
