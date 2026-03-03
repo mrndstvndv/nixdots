b387e0b474ab5e10bf74274eb3e38e1f95883be2
Steven <steven@example.com>
2026-03-03 00:25:42 +0000

    neovim: simplify to always use my-neovim

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,15 +1,6 @@
-{ pkgs, lib, my-neovim ? null, ... }:
-let
-  system = pkgs.stdenv.hostPlatform.system;
-  useMyNeovim =
-    my-neovim != null
-    && my-neovim ? packages
-    && builtins.hasAttr system my-neovim.packages;
-in
+{ pkgs, my-neovim, ... }:
 {
-  home.packages =
-    if useMyNeovim then
-      [ my-neovim.packages.${system}.default ]
-    else
-      [ pkgs.neovim ];
+  home.packages = [
+    my-neovim.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
 }
