38955102e0ceb7834a425ae61888606b5c6e3433
steven <steven@proputer.local>
2026-07-12 05:43:12 +0800

    flake: add fish to shells

--- flake.nix
+++ flake.nix
@@ -162,6 +162,7 @@
 
       # Enable alternative shell support in nix-darwin.
       programs.fish.enable = true;
+      environment.shells = [ pkgs.fish ];
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
