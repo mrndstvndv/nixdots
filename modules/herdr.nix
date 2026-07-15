df2676b20f5fac013e1484ab0d56c07799cfa598
mrndstvndv <stvndvmrnd@gmail.com>
2026-07-15 16:26:54 +0800

    herdr: some improvements

--- modules/herdr.nix
+++ modules/herdr.nix
@@ -3,30 +3,41 @@
   home.file.".config/herdr/config.toml".text = ''
     onboarding = false
 
-    [theme]
-    name = "terminal"
-
     [ui]
     prompt_new_tab_name = false
+    show_agent_labels_on_pane_borders = false
 
     [keys]
-    focus_pane_right = ""
-    detach = "prefix+d"
+    settings = ""
+    workspace_picker = "prefix+s"
+    previous_workspace = "prefix+("
+    next_workspace = "prefix+)"
     split_vertical = "prefix+%"
-    split_horizontal = "prefix+\""
-
-    [terminal]
-    default_shell = "fish"
-    shell_mode = "auto"
-    new_cwd = "follow"
+    split_horizontal = 'prefix+"'
+    detach = ["prefix+q", "prefix+d"]
+    focus_pane_right = ""
 
     [[keys.command]]
     key = "prefix+l"
     type = "pane"
     command = "lazygit"
-    description = "open lazygit"
+
+    [ui.toast]
+    delivery = "terminal"
+
+    [ui.sound]
+    enabled = true
+
+    [experimental]
+    pane_history = true
+
+    [terminal]
+    default_shell = "fish"
 
     [session]
     resume_agents_on_restore = true
+
+    [theme]
+    name = "terminal"
   '';
 }
